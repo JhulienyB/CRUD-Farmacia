fc40e6aeb5869c4064ffd13a416eb9f2281347dd
JhulienyB <jhulienybucci@gmail.com>
2024-05-03 10:41:49 -0300

    Configurando a conexão com o Banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_farmacia',
+      entities: [/*Categoria, Produto*/],
+      synchronize: true
+    }),
+    //CategoriaModule,
+    //ProdutoModule,
+  ],
   controllers: [],
   providers: [],
 })
